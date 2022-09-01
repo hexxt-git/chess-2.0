9d7050ca6140456dd5ef4a8c077c19d910595ba3
9EED <zeghdns@gmail.com>
2022-09-01 18:53:50 +0100

    *

---
+++ index.js
@@ -0,0 +1,9 @@
+continueGame = $('continue')
+play = $('play')
+faq = $('faq')
+settings = $('settings')
+credits = $('credits')
+
+play.addEventListener('click', ()=>{
+    window.location = './game setup.html'
+})
